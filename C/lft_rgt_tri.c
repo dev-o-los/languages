953dc5d6b7d42beb898c8609536f8c61035fa64b
devolos <utk27032005@gmail.com>
2024-08-18 17:03:09 +0530

    pattern programs

---
+++ C/lft_rgt_tri.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int rows, i, j, space;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+
+    for (i = 1; i <= rows; ++i)
+    {
+        // Print spaces and stars in a single loop
+        for (j = 1; j <= rows; ++j)
+        {
+            if (j <= rows - i)
+                printf("  ");
+            else
+                printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ C/up_down_inv_tri.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int rows, i, j, space;
+    rows = 5;
+
+    for (i = 1; i <= rows; ++i)
+    {
+        // Print spaces and stars in a single loop
+        for (j = 1; j <= rows; ++j)
+        {
+            if (j <= rows - i)
+                printf("  ");
+            else
+                printf("* ");
+        }
+        printf("\n");
+    }
+    for (i = 1; i <= rows; ++i)
+    {
+        // Print spaces and stars in a single loop
+        for (j = 1; j <= rows - 1; ++j)
+        {
+            if (j < i)
+                printf("  ");
+            else
+                printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
